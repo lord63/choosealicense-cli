d9d0243c3cb058860018631b3dd3a9938a721dce
lord63 <lord63.j@gmail.com>
2015-04-13 09:06:07 +0800

    Use unittest for test_show.py
    for better debugging info

--- choosealicense/test/test_show.py
+++ choosealicense/test/test_show.py
@@ -5,16 +5,19 @@
     Tests for the `license show` function
 """
 
+import unittest
+
 from click.testing import CliRunner
 
 from choosealicense.main import show
 
 
-def test_show_all_the_licenses():
-    runner = CliRunner()
-    result = runner.invoke(show)
-    it_should_be = ('agpl-3.0, apache-2.0, artistic-2.0, bsd-2-clause, '
-                    'bsd-3-clause, cc0-1.0, epl-1.0, gpl-2.0, gpl-3.0, isc, '
-                    'lgpl-2.1, lgpl-3.0, mit, mpl-2.0, unlicense')
-    assert result.exit_code == 0
-    assert result.output.strip() == it_should_be
+class TestShow(unittest.TestCase):
+    def test_show_all_the_licenses(self):
+        runner = CliRunner()
+        result = runner.invoke(show)
+        it_should_be = ('agpl-3.0, apache-2.0, artistic-2.0, bsd-2-clause, '
+                        'bsd-3-clause, cc0-1.0, epl-1.0, gpl-2.0, gpl-3.0, '
+                        'isc, lgpl-2.1, lgpl-3.0, mit, mpl-2.0, unlicense')
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output.strip(), it_should_be)
