fa85684a513bcb7ea7af5040774dfe0a9521737b
lord63 <lord63.j@gmail.com>
2016-03-19 09:27:42 +0800

    DRY, move get git info into a function

--- choosealicense/utils.py
+++ choosealicense/utils.py
@@ -16,20 +16,22 @@ send_request = partial(
     headers={'accept': 'application/vnd.github.drax-preview+json'})
 
 
+def _get_git_info(name):
+    """Get git configuration info, e.g. user.email."""
+    try:
+        command_list = ('git config --get user.{0}'.format(name)).split()
+        result = subprocess.check_output(command_list).strip().decode('utf-8')
+    except subprocess.CalledProcessError:
+        raise click.ClickException(
+            "Please configure your git(user.{0}).".format(name))
+    else:
+        return result
+
+
 def get_default_context():
     """Get the default context for the license template."""
     year = str(date.today().year)
-    try:
-        fullname = subprocess.check_output(
-            'git config --get user.name'.split()
-        ).strip().decode('utf-8')
-    except subprocess.CalledProcessError:
-        raise click.ClickException("Please configure your git(user.name).")
-    try:
-        email = subprocess.check_output(
-            'git config --get user.email'.split()
-        ).strip().decode('utf-8')
-    except subprocess.CalledProcessError:
-        raise click.ClickException("Please configure your git(user.email).")
+    fullname = _get_git_info('name')
+    email = _get_git_info('email')
     return {'year': year, 'fullname': fullname, 'email': email,
             'project': 'the copyright holder'}
